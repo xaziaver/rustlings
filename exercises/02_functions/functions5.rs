7977078181d565e0463f6561f1c6efe87232a009
xaziaver <61274673+xaziaver@users.noreply.github.com>
2024-02-01 23:02:20 +0000

    functions

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint functions5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let answer = square(3);
@@ -11,5 +10,13 @@ fn main() {
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+
+    // removing semi-colon makes the function
+    // return the value of this expression
+    num * num
+    // the value of the last expression in
+    // a block {} is automatically returned
+
+    // shorthand for
+    // return num * num;
 }
