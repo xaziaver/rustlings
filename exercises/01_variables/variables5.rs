3f2807f3fc6a8176ceafcacdca8eb49876fcd068
xaziaver <61274673+xaziaver@users.noreply.github.com>
2024-02-01 11:39:09 +0000

    finish variables

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,11 +3,24 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
-    println!("Number plus two is : {}", number + 2);
+
+    /* 
+        using `let` here allows the use of "shadowing" in Rust
+        where you can reassign the value and type of a named variable
+        without needing to make it mutable
+    */
+    let number = 3; // don't rename this 
+    // we can change type because a new variable is effectively created
+    {
+        let number = number + 5;
+        println!("Number plus two inner scope is : {}", number + 2);
+    }
+    /*
+        shadowing is valid until the end of the scope
+    */
+    println!("Number plus two outer scope is : {}", number + 2);
 }
--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,9 +3,15 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-const NUMBER = 3;
+/*
+    const differences:
+        (1) can't use mut keyword, always immutable
+        (1) declare with const, uppercase, underscores
+        (3) the type MUST be annotated, not inferred 
+        (4) may only be set to a constant expression 
+*/
+const NUMBER_I_PICKED: i32 = 3;
 fn main() {
-    println!("Number {}", NUMBER);
+    println!("Number {}", (NUMBER_I_PICKED * 2) + 2);
 }
