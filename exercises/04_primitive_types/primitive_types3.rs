0967e1863b1893ae5947dd7fbfba6311a73791df
xaziaver <61274673+xaziaver@users.noreply.github.com>
2024-02-02 00:19:36 +0000

    primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,15 +5,20 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    // array with 100 elements with the value 0
+    let a = [0; 100];
+    // aray with explicit identifier 
+    // 5 i32 elements, 1 to 5
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
+    } else if a.len() == 5 {
+        println!("Array has {} values {} to {}", a.len(), a[0], a[a.len() - 1]);
     } else {
-        println!("Meh, I eat arrays like that for breakfast.");
+        println!("Meh, I eat arrays with size {} for breakfast.", a.len());
         panic!("Array not big enough, more elements needed")
     }
 }
--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -5,11 +5,12 @@
 // Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+    // name is initialized with first value in tuple
+    // age is initialized with second value in tuple
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
